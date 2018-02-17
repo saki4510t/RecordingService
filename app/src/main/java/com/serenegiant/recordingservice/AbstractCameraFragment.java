4392821f22ef3c6007065eb9c1c6d182e321f184
saki4510t <saki4510t@gmail.com>
2018-02-17 18:02:08 +0900

    typo

--- app/src/main/java/com/serenegiant/recordingservice/AbstractCameraFragment.java
+++ app/src/main/java/com/serenegiant/recordingservice/AbstractCameraFragment.java
@@ -3,7 +3,7 @@ package com.serenegiant.recordingservice;
  *
  * Copyright (c) 2016-2018 saki t_saki@serenegiant.com
  *
- * File name: CameraFragment.java
+ * File name: AbstractCameraFragment.java
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
