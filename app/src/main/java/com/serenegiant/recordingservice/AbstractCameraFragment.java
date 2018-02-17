c2f2996c2e10f3046b7220467916ca17fa066680
saki4510t <saki4510t@gmail.com>
2018-02-17 17:48:45 +0900

    テスト用アプリのCameraFragmentを基本機能を実装するAbstractCameraFragmentとそれを継承して実際の録画開始終了処理を実装するCameraFragmentに分割

---
+++ app/src/main/java/com/serenegiant/recordingservice/AbstractCameraFragment.java
@@ -0,0 +1,169 @@
+package com.serenegiant.recordingservice;
+/*
+ *
+ * Copyright (c) 2016-2018 saki t_saki@serenegiant.com
+ *
+ * File name: CameraFragment.java
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * All files in the folder are under this Apache License, Version 2.0.
+*/
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * 内蔵カメラへアクセスして表示するための基本クラス
+ * 録画の開始/停止の実際の処理以外を実装
+ */
+public abstract class AbstractCameraFragment extends Fragment {
+	private static final boolean DEBUG = true;	// TODO set false on release
+	private static final String TAG = AbstractCameraFragment.class.getSimpleName();
+
+	/**
+	 * video resolution
+	 */
+	protected static final int VIDEO_WIDTH = 1280, VIDEO_HEIGHT = 720;
+	
+	/**
+	 * for camera preview display
+	 */
+	protected CameraGLView mCameraView;
+	/**
+	 * for scale mode display
+	 */
+	private TextView mScaleModeView;
+	/**
+	 * button for start/stop recording
+	 */
+	private ImageButton mRecordButton;
+
+	public AbstractCameraFragment() {
+		super();
+		// デフォルトコンストラクタが必要
+	}
+
+	@Override
+	public View onCreateView(final LayoutInflater inflater,
+							 final ViewGroup container, final Bundle savedInstanceState) {
+
+		final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+		mCameraView = rootView.findViewById(R.id.cameraView);
+		mCameraView.setVideoSize(VIDEO_WIDTH, VIDEO_HEIGHT);
+		mCameraView.setOnClickListener(mOnClickListener);
+		mScaleModeView = rootView.findViewById(R.id.scalemode_textview);
+		updateScaleModeText();
+		mRecordButton = rootView.findViewById(R.id.record_button);
+		mRecordButton.setOnClickListener(mOnClickListener);
+		return rootView;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (DEBUG) Log.v(TAG, "onResume:");
+		mCameraView.onResume();
+		mCameraView.addListener(mOnFrameAvailableListener);
+	}
+
+	@Override
+	public void onPause() {
+		if (DEBUG) Log.v(TAG, "onPause:");
+		stopRecording();
+		mCameraView.removeListener(mOnFrameAvailableListener);
+		mCameraView.onPause();
+		super.onPause();
+	}
+
+	/**
+	 * method when touch record button
+	 */
+	private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(final View view) {
+			switch (view.getId()) {
+			case R.id.cameraView:
+				final int scale_mode = (mCameraView.getScaleMode() + 1) % 4;
+				mCameraView.setScaleMode(scale_mode);
+				updateScaleModeText();
+				break;
+			case R.id.record_button:
+				if (!isRecording()) {
+					startRecording();
+				} else {
+					stopRecording();
+				}
+				break;
+			}
+		}
+	};
+
+	private void updateScaleModeText() {
+		final int scale_mode = mCameraView.getScaleMode();
+		mScaleModeView.setText(
+			scale_mode == 0 ? "scale to fit"
+			: (scale_mode == 1 ? "keep aspect(viewport)"
+			: (scale_mode == 2 ? "keep aspect(matrix)"
+			: (scale_mode == 3 ? "keep aspect(crop center)" : ""))));
+	}
+
+	protected abstract boolean isRecording();
+	
+	/**
+	 * start recording
+	 * This is a sample project and call this on UI thread to avoid being complicated
+	 * but basically this should be called on private thread because prepareing
+	 * of encoder is heavy work
+	 */
+	protected void startRecording() {
+		if (DEBUG) Log.v(TAG, "startRecording:");
+		mRecordButton.setColorFilter(0xffff0000);	// turn red
+		try {
+			// FIXME 未実装 ちゃんとパーミッションのチェック＆要求をしないとだめ
+			internalStartRecording();
+		} catch (final Exception e) {
+			mRecordButton.setColorFilter(0);
+			Log.e(TAG, "startCapture:", e);
+		}
+	}
+
+	protected abstract void internalStartRecording();
+	
+	/**
+	 * request stop recording
+	 */
+	protected void stopRecording() {
+		if (DEBUG) Log.v(TAG, "stopRecording:");
+		mRecordButton.setColorFilter(0);	// return to default color
+		internalStopRecording();
+	}
+
+	protected abstract void internalStopRecording();
+	
+	private final CameraGLView.OnFrameAvailableListener
+		mOnFrameAvailableListener = new CameraGLView.OnFrameAvailableListener() {
+		@Override
+		public void onFrameAvailable() {
+			AbstractCameraFragment.this.onFrameAvailable();
+		}
+	};
+	
+	protected abstract void onFrameAvailable();
+}
--- app/src/main/java/com/serenegiant/recordingservice/CameraFragment.java
+++ app/src/main/java/com/serenegiant/recordingservice/CameraFragment.java
@@ -20,17 +20,9 @@ package com.serenegiant.recordingservice;
  * All files in the folder are under this Apache License, Version 2.0.
 */
 
-import android.app.Fragment;
-import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Surface;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
 import com.serenegiant.service.AbstractServiceRecorder;
 import com.serenegiant.service.PostMuxRecService;
@@ -39,134 +31,44 @@ import com.serenegiant.utils.FileUtils;
 
 import java.io.File;
 
-public class CameraFragment extends Fragment {
+public class CameraFragment extends AbstractCameraFragment {
 	private static final boolean DEBUG = true;	// TODO set false on release
 	private static final String TAG = CameraFragment.class.getSimpleName();
 	
-	/**
-	 * video resolution
-	 */
-	private static final int VIDEO_WIDTH = 1280, VIDEO_HEIGHT = 720;
-	
-	/**
-	 * for camera preview display
-	 */
-	private CameraGLView mCameraView;
-	/**
-	 * for scale mode display
-	 */
-	private TextView mScaleModeView;
-	/**
-	 * button for start/stop recording
-	 */
-	private ImageButton mRecordButton;
-
 	private PostMuxRecorder mPostMuxRecorder;
 	
 	public CameraFragment() {
+		super();
 		// need default constructor
 	}
 
 	@Override
-	public View onCreateView(final LayoutInflater inflater,
-		final ViewGroup container, final Bundle savedInstanceState) {
-
-		final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-		mCameraView = rootView.findViewById(R.id.cameraView);
-		mCameraView.setVideoSize(VIDEO_WIDTH, VIDEO_HEIGHT);
-		mCameraView.setOnClickListener(mOnClickListener);
-		mScaleModeView = rootView.findViewById(R.id.scalemode_textview);
-		updateScaleModeText();
-		mRecordButton = rootView.findViewById(R.id.record_button);
-		mRecordButton.setOnClickListener(mOnClickListener);
-		return rootView;
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		if (DEBUG) Log.v(TAG, "onResume:");
-		mCameraView.onResume();
-		mCameraView.addListener(mOnFrameAvailableListener);
-	}
-
-	@Override
-	public void onPause() {
-		if (DEBUG) Log.v(TAG, "onPause:");
-		stopRecording();
-		mCameraView.removeListener(mOnFrameAvailableListener);
-		mCameraView.onPause();
-		super.onPause();
-	}
-
-	/**
-	 * method when touch record button
-	 */
-	private final OnClickListener mOnClickListener = new OnClickListener() {
-		@Override
-		public void onClick(final View view) {
-			switch (view.getId()) {
-			case R.id.cameraView:
-				final int scale_mode = (mCameraView.getScaleMode() + 1) % 4;
-				mCameraView.setScaleMode(scale_mode);
-				updateScaleModeText();
-				break;
-			case R.id.record_button:
-				if (!isRecording()) {
-					startRecording();
-				} else {
-					stopRecording();
-				}
-				break;
-			}
-		}
-	};
-
-	private void updateScaleModeText() {
-		final int scale_mode = mCameraView.getScaleMode();
-		mScaleModeView.setText(
-			scale_mode == 0 ? "scale to fit"
-			: (scale_mode == 1 ? "keep aspect(viewport)"
-			: (scale_mode == 2 ? "keep aspect(matrix)"
-			: (scale_mode == 3 ? "keep aspect(crop center)" : ""))));
-	}
-
-	private boolean isRecording() {
+	protected boolean isRecording() {
 		return mPostMuxRecorder != null;
 	}
-	
-	/**
-	 * start recording
-	 * This is a sample project and call this on UI thread to avoid being complicated
-	 * but basically this should be called on private thread because prepareing
-	 * of encoder is heavy work
-	 */
-	private void startRecording() {
-		if (DEBUG) Log.v(TAG, "startRecording:");
-		mRecordButton.setColorFilter(0xffff0000);	// turn red
-		try {
-			// FIXME 未実装 ちゃんとパーミッションのチェック＆要求をしないとだめ
-			if (mPostMuxRecorder == null) {
-				mPostMuxRecorder = PostMuxRecorder.newInstance(getActivity(),
-					PostMuxRecService.class, mCallback,
-					PostMuxRecService.MUX_INTERMEDIATE_TYPE_CHANNEL);
-				
-			}
-		} catch (final Exception e) {
-			mRecordButton.setColorFilter(0);
-			Log.e(TAG, "startCapture:", e);
+
+	@Override
+	protected void internalStartRecording() {
+		if (mPostMuxRecorder == null) {
+			mPostMuxRecorder = PostMuxRecorder.newInstance(getActivity(),
+				PostMuxRecService.class, mCallback,
+				PostMuxRecService.MUX_INTERMEDIATE_TYPE_CHANNEL);
+			
 		}
 	}
 
-	/**
-	 * request stop recording
-	 */
-	private void stopRecording() {
-		if (DEBUG) Log.v(TAG, "stopRecording:");
-		mRecordButton.setColorFilter(0);	// return to default color
+	@Override
+	protected void internalStopRecording() {
 		if (mPostMuxRecorder != null) {
 			mPostMuxRecorder.release();
 			mPostMuxRecorder = null;
+		}
+	}
+
+	@Override
+	protected void onFrameAvailable() {
+		if (mPostMuxRecorder != null) {
+			mPostMuxRecorder.frameAvailableSoon();
 		}
 	}
 
@@ -238,17 +140,8 @@ public class CameraFragment extends Fragment {
 				mCameraView.removeSurface(mRecordingSurfaceId);
 				mRecordingSurfaceId = 0;
 			}
-			mRecordButton.setColorFilter(0);	// return to default color
+			stopRecording();
 		}
 	};
-	
-	private final CameraGLView.OnFrameAvailableListener
-		mOnFrameAvailableListener = new CameraGLView.OnFrameAvailableListener() {
-		@Override
-		public void onFrameAvailable() {
-			if (mPostMuxRecorder != null) {
-				mPostMuxRecorder.frameAvailableSoon();
-			}
-		}
-	};
+
 }
