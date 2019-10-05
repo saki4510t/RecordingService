8f52fd2a34111cf33e8651a72030854f776c9e9d
saki4510t <saki4510t@gmail.com>
2019-10-05 11:35:27 +0900

    SimpleRecorderServiceを実装

--- librecservice/src/main/java/com/serenegiant/service/SimpleRecorderService.java
+++ librecservice/src/main/java/com/serenegiant/service/SimpleRecorderService.java
@@ -1,13 +1,21 @@
 package com.serenegiant.service;
 
+import android.annotation.SuppressLint;
 import android.media.MediaCodec;
 import android.media.MediaFormat;
+import android.media.MediaMuxer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.text.TextUtils;
 import android.util.Log;
 
+import com.serenegiant.media.IMuxer;
+import com.serenegiant.media.MediaMuxerWrapper;
 import com.serenegiant.media.MediaReaper;
+import com.serenegiant.utils.BuildCheck;
+import com.serenegiant.utils.UriHelper;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
@@ -19,7 +27,7 @@ import androidx.documentfile.provider.DocumentFile;
  * 録画機能をサービス側で実行するだけのサービス
  */
 public class SimpleRecorderService extends AbstractRecorderService {
-	private static final boolean DEBUG = false;	// FIXME set false on production
+	private static final boolean DEBUG = true;	// FIXME set false on production
 	private static final String TAG = SimpleRecorderService.class.getSimpleName();
 
 	/** Binder class to access this local service */
@@ -31,12 +39,25 @@ public class SimpleRecorderService extends AbstractRecorderService {
 
 	/** binder instance to access this local service */
 	private final IBinder mBinder = new LocalBinder();
+	private String mOutputPath;
+	private IMuxer mMuxer;
+	private int mVideoTrackIx = -1;
+	private int mAudioTrackIx = -1;
 
 	@Override
 	protected IBinder getBinder() {
 		return mBinder;
 	}
 
+	/**
+	 * #startの実態, mSyncをロックして呼ばれる
+	 * @param outputDir 出力ディレクトリ
+	 * @param name 出力ファイル名(拡張子なし)
+	 * @param videoFormat
+	 * @param audioFormat
+	 * @throws IOException
+	 */
+	@SuppressLint("InlinedApi")
 	@Override
 	protected void internalStart(
 		@NonNull final String outputDir,
@@ -45,8 +66,30 @@ public class SimpleRecorderService extends AbstractRecorderService {
 		@Nullable final MediaFormat audioFormat) throws IOException {
 
 		if (DEBUG) Log.v(TAG, "internalStart:");
+		if (!TextUtils.isEmpty(outputDir) && !TextUtils.isEmpty(name)) {
+			final String outputPath
+				= outputDir + (outputDir.endsWith("/")
+					? name : "/" + name) + ".mp4";
+
+			mMuxer = new MediaMuxerWrapper(
+				outputPath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+			mVideoTrackIx = videoFormat != null ? mMuxer.addTrack(videoFormat) : -1;
+			mAudioTrackIx = audioFormat != null ? mMuxer.addTrack(audioFormat) : -1;
+			mMuxer.start();
+		} else {
+			throw new IOException("invalid output dir or name");
+		}
 	}
 
+	/**
+	 * #startの実態, mSyncをロックして呼ばれる
+	 * @param outputDir 出力ディレクトリ
+	 * @param name 出力ファイル名(拡張子なし)
+	 * @param videoFormat
+	 * @param audioFormat
+	 * @throws IOException
+	 */
+	@SuppressLint("NewApi")
 	@Override
 	protected void internalStart(
 		@NonNull final DocumentFile outputDir,
@@ -55,6 +98,57 @@ public class SimpleRecorderService extends AbstractRecorderService {
 		@Nullable final MediaFormat audioFormat) throws IOException {
 
 		if (DEBUG) Log.v(TAG, "internalStart:");
+		final DocumentFile output = outputDir.createFile("*/*", name + ".mp4");
+		IMuxer muxer = null;
+		if (BuildCheck.isOreo()) {
+			muxer = new MediaMuxerWrapper(getContentResolver()
+				.openFileDescriptor(output.getUri(), "rw").getFileDescriptor(),
+				MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+		} else {
+			final String path = UriHelper.getPath(this, output.getUri());
+			final File f = new File(UriHelper.getPath(this, output.getUri()));
+			if (/*!f.exists() &&*/ f.canWrite()) {
+				// 書き込めるファイルパスを取得できればそれを使う
+				muxer = new MediaMuxerWrapper(path,
+					MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+			} else {
+				Log.w(TAG, "cant't write to the file, try to use VideoMuxer instead");
+			}
+		}
+		if (muxer == null) {
+			throw new IllegalArgumentException();
+		}
+		mMuxer = muxer;
+		mVideoTrackIx = videoFormat != null ? muxer.addTrack(videoFormat) : -1;
+		mAudioTrackIx = audioFormat != null ? muxer.addTrack(audioFormat) : -1;
+		mMuxer.start();
+	}
+
+	@Override
+	protected void internalStop() {
+		if (DEBUG) Log.v(TAG, "internalStop:");
+		if (mMuxer != null) {
+			final IMuxer muxer = mMuxer;
+			mMuxer = null;
+			try {
+				muxer.stop();
+			} catch (final Exception e) {
+				Log.w(TAG, e);
+			}
+			try {
+				muxer.release();
+			} catch (final Exception e) {
+				Log.w(TAG, e);
+			}
+		}
+		if (getState() == STATE_RECORDING) {
+			setState(STATE_READY);
+			if (!TextUtils.isEmpty(mOutputPath)) {
+				final String path = mOutputPath;
+				mOutputPath = null;
+				scanFile(path);
+			}
+		}
 	}
 
 	@Override
@@ -64,11 +158,17 @@ public class SimpleRecorderService extends AbstractRecorderService {
 		@NonNull final MediaCodec.BufferInfo bufferInfo,
 		final long ptsUs) throws IOException {
 
-		if (DEBUG) Log.v(TAG, "onWriteSampleData:");
+//		if (DEBUG) Log.v(TAG, "onWriteSampleData:");
+		if (mMuxer != null) {
+			switch (reaper.reaperType()) {
+			case MediaReaper.REAPER_VIDEO:
+				mMuxer.writeSampleData(mVideoTrackIx, byteBuf, bufferInfo);
+				break;
+			case MediaReaper.REAPER_AUDIO:
+				mMuxer.writeSampleData(mAudioTrackIx, byteBuf, bufferInfo);
+				break;
+			}
+		}
 	}
 
-	@Override
-	protected void internalStop() {
-		if (DEBUG) Log.v(TAG, "internalStop:");
-	}
 }
